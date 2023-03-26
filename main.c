b8e511756493ea8ab4f18aa9fc9b59b2242112fa
MorpKnight <christoffelsihombing@gmail.com>
2023-03-26 21:37:01 +0700

    Add sorting algorithm

--- main.c
+++ main.c
@@ -2,9 +2,149 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
+#include <time.h>
+
+void randomList(int *list, int n){
+    srand(time(NULL));
+    for (int i = 0; i < n; i++){
+        list[i] = rand() % 100;
+    }
+}
+
+void BubbleSort(int *list, int n){
+    int temp;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n - i - 1; j++){
+            if (list[j] > list[j + 1]){
+                temp = list[j];
+                list[j] = list[j + 1];
+                list[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void SelectionSort(int *list, int n){
+    int temp, min;
+    for (int i = 0; i < n; i++){
+        min = i;
+        for (int j = i + 1; j < n; j++){
+            if (list[j] < list[min]){
+                min = j;
+            }
+        }
+        temp = list[i];
+        list[i] = list[min];
+        list[min] = temp;
+    }
+}
+
+void InsertionSort(int *list, int n){
+    int temp, j;
+    for (int i = 1; i < n; i++){
+        temp = list[i];
+        j = i - 1;
+        while (j >= 0 && list[j] > temp){
+            list[j + 1] = list[j];
+            j--;
+        }
+        list[j + 1] = temp;
+    }
+}
+
+void Merge(int *list, int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++){
+        L[i] = list[l + i];
+    }
+    for (j = 0; j < n2; j++){
+        R[j] = list[m + 1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2){
+        if (L[i] <= R[j]){
+            list[k] = L[i];
+            i++;
+        }
+        else {
+            list[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1){
+        list[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2){
+        list[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void MergeSort(int *list, int l, int r){
+    if (l < r){
+        int m = l + (r - l) / 2;
+        MergeSort(list, l, m);
+        MergeSort(list, m + 1, r);
+        Merge(list, l, m, r);
+    }
+}
+
+void QuickSort(int *list, int l, int r){
+    int i = l;
+    int j = r;
+    int temp;
+    int pivot = list[(l + r) / 2];
+
+    while (i <= j){
+        while (list[i] < pivot){
+            i++;
+        }
+        while (list[j] > pivot){
+            j--;
+        }
+        if (i <= j){
+            temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    if (l < j){
+        QuickSort(list, l, j);
+    }
+    if (i < r){
+        QuickSort(list, i, r);
+    }
+}
 
 int main(){
-    int mode, algoType;
+    int mode, algoType, n;
+
+    // generate random list of numbers
+    printf("Masukkan jumlah data: ");
+    scanf("%d", &n);
+    int list[n];
+    randomList(list, n);
+    for (int i = 0; i < n; i++){
+        printf("%d ", list[i]);
+    }
+    printf("\n");
 
     do {
         printf("Pilih mode:\n1. Sorting\n2. Searching\nPilih: ");
@@ -15,14 +155,19 @@ int main(){
                 scanf("%d", &algoType);
                 switch(algoType){
                     case 1:
+                        BubbleSort(list, n);
                         break;
                     case 2:
+                        SelectionSort(list, n);
                         break;
                     case 3:
+                        InsertionSort(list, n);
                         break;
                     case 4:
+                        MergeSort(list, 0, n - 1);
                         break;
                     case 5:
+                        QuickSort(list, 0, n - 1);
                         break;
                     default:
                         system("cls");
@@ -50,6 +195,10 @@ int main(){
                 break;
         }
     } while (mode != 1 && mode != 2);
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", list[i]);
+    }
 
     return 0;
 }
