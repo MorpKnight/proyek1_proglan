7ad1d2f9110413c7293d0724e3fb7e58b4e22d7d
MorpKnight <christoffelsihombing@gmail.com>
2023-03-29 20:21:16 +0700

    Add sorting, asktoplay, and more

--- main.c
+++ main.c
@@ -40,10 +40,90 @@ void testPrint(SONG *data, int count){
     }
 }
 
-void play(char *link){
+void printTitleSingeronly(SONG *data, int count){
+    int i;
+
+    printf("Daftar lagu:\n");
+    for(i = 0; i < count; i++){
+        printf("%d. %s - %s\n", i+1, data[i].title, data[i].singer);
+    }
+}
+
+void searchSongbyGenre(SONG *data, int count){
+    char genre[100];
+    int i, j, found;
+
+    printf("Masukkan genre: ");
+    scanf("%s", genre);
+
+    found = 0;
+    for(i = 0; i < count; i++){
+        j = 0;
+        while(strcmp(data[i].genres[j], "") != 0){
+            if(strcmp(data[i].genres[j], genre) == 0){
+                printf("%d. %s - %s\n", i+1, data[i].title, data[i].singer);
+                found = 1;
+                break;
+            }
+            j++;
+        }
+    }
+
+    if(found == 0){
+        printf("Tidak ada lagu dengan genre %s\n", genre);
+    }
+}
+
+void playSong(char *link){
     char command[256];
     snprintf(command, sizeof(command), "start %s", link);
     system(command);
+}
+
+void sortList(SONG *data, int count){
+    int i, j;
+    SONG temp;
+    for(i = 0; i < count; i++){
+        for(j = i+1; j < count; j++){
+            if(strcmp(data[i].title, data[j].title) > 0){
+                temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+    }
+}
+
+void askToPlaySong(SONG *data, int count){
+    int i, j, found;
+
+    printf("Masukkan judul lagu: ");
+    char title[100];
+    scanf("%s", title);
+
+    found = 0;
+    for(i = 0; i < count; i++){
+        if(strcmp(data[i].title, title) == 0){
+            printf("Apakah anda ingin memutar lagu %s (y/n): ", data[i].title);
+            char play;
+            do {
+                scanf(" %c", &play);
+                if(play != 'y' && play != 'n'){
+                    printf("Masukkan y atau n: ");
+                }
+            } while(play != 'y' && play != 'n');
+
+            if(play == 'y'){
+                playSong(data[i].link);
+            }
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0){
+        printf("Tidak ada lagu dengan judul %s\n", title);
+    }
 }
 
 int main(){
@@ -80,14 +160,62 @@ int main(){
     // split genre to genres that separated with ","
     splitGenreToGenres(data, count);
 
-    printf("Selamat datang di Music Player!\n");
-    printf("1. Tampilkan semua lagu\n");
-    printf("2. Tampilkan lagu berdasarkan genre\n");
-    printf("3. Tampilkan lagu berdasarkan tahun rilis\n");
-    printf("4. Tampilkan lagu berdasarkan penyanyi\n");
-    printf("5. Tampilkan lagu berdasarkan judul\n");
-    printf("6. Keluar program\n");
-    printf("Masukkan mode: ");
+    menu:
+        printf("Selamat datang di Music Player!\n");
+        printf("1. Tampilkan semua lagu\n");
+        printf("2. Tampilkan lagu berdasarkan genre\n");
+        printf("3. Tampilkan lagu berdasarkan tahun rilis\n");
+        printf("4. Tampilkan lagu berdasarkan penyanyi\n");
+        printf("5. Tampilkan lagu berdasarkan judul\n");
+        printf("6. Keluar program\n");
+        printf("Masukkan mode: ");
+
+    do {
+        scanf("%d", &mode);
+        if(mode < 1 || mode > 6){
+            printf("Masukkan mode yang benar: ");
+        }
+    } while(mode < 1 || mode > 6);
+
+    switch(mode){
+        case 1:
+            system("cls");
+            printTitleSingeronly(data, count);
+            
+            printf("Apakah list ingin di sort berdasarkan abjad (y/n): ");
+            char sort;
+            do {
+                scanf(" %c", &sort);
+                if(sort != 'y' && sort != 'n'){
+                    printf("Masukkan y atau n: ");
+                }
+            } while(sort != 'y' && sort != 'n');
+
+            if(sort == 'y'){
+                sortList(data, count);
+                printTitleSingeronly(data, count);
+            }
+
+            askToPlaySong(data, count);
+            break;
+        case 2:
+            system("cls");
+            searchSongbyGenre(data, count);
+            goto menu;
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            printf("\nTerima kasih telah menggunakan Music Player!\n");
+            return 0;
+        default:
+            printf("Masukkan mode yang benar\n");
+            return 0;
+    }
 
     return 0;
 }
